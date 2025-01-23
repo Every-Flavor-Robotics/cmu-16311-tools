615e5850f89622d6721d265bb1c682be55679eb0
Swapnil Pande <swapnil.pande98@gmail.com>
2025-01-23 14:06:40 -0500

    Added range sensor install to install script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 REPO_URL="https://github.com/Every-Flavor-Robotics/cmu-16311-tools.git"
 MOTORGO_PYTHON_DIR="motorgo-python"
 BH1750_DIR="Adafruit_CircuitPython_BH1750"
-
+VL53L4CX_DIR="Adafruit_CircuitPython_VL53L4CX"
 
 # Setup install flags
 # If python version is 3.12 or greater, add --break-system-packages flag
@@ -33,6 +33,10 @@ pip install $EXTRA_PIP_FLAGS .
 
 # Repeart for the BH1750 library
 cd "$TMP_DIR/$BH1750_DIR"
+pip install $EXTRA_PIP_FLAGS .
+
+# Repeart for the VL53L4CX library
+cd "$TMP_DIR/$VL53L4CX_DIR"
 pip install $EXTRA_PIP_FLAGS .
 
 # Move back out of the temp directory
