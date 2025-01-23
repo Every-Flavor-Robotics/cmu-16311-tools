cee96538cd722a86396adec7687d1d4fe6d3a96b
Swapnil Pande <swapnil.pande98@gmail.com>
2025-01-23 11:42:15 -0500

    Added bh1750 lib to install

--- install.sh
+++ install.sh
@@ -8,6 +8,17 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 REPO_URL="https://github.com/Every-Flavor-Robotics/cmu-16311-tools.git"
 MOTORGO_PYTHON_DIR="motorgo-python"
+BH1750_DIR="Adafruit_CircuitPython_BH1750"
+
+
+# Setup install flags
+# If python version is 3.12 or greater, add --break-system-packages flag
+EXTRA_PIP_FLAGS=""
+# Check if python version is 3.12 or greater
+if python -c 'import sys; exit(not (sys.version_info.major == 3 and sys.version_info.minor >= 12))'; then
+    # Add --break-system-packages flag to install command
+    EXTRA_PIP_FLAGS="--break-system-packages"
+fi
 
 # Create a temp directory
 TMP_DIR=$(mktemp -d)
@@ -15,18 +26,14 @@ TMP_DIR=$(mktemp -d)
 # Clone the repo to the temp directory
 git clone "$REPO_URL" "$TMP_DIR"
 
-# Move into the cloned directory
+
+# Move into the MotorGo Python dir and install the package
 cd "$TMP_DIR/$MOTORGO_PYTHON_DIR"
+pip install $EXTRA_PIP_FLAGS .
 
-
-# Check if python version is 3.12 or greater
-if python -c 'import sys; exit(not (sys.version_info.major == 3 and sys.version_info.minor >= 12))'; then
-    # Add --break-system-packages flag to install command
-    pip install --break-system-packages .
-else
-    pip install .
-fi
-
+# Repeart for the BH1750 library
+cd "$TMP_DIR/$BH1750_DIR"
+pip install $EXTRA_PIP_FLAGS .
 
 # Move back out of the temp directory
 cd -
