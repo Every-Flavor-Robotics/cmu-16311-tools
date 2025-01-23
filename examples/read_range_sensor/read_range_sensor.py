e1ee819f4c9ae55280400a4e19126f0cb9584b51
Swapnil Pande <swapnil.pande98@gmail.com>
2025-01-23 14:29:12 -0500

    Simplified range sensor example

--- examples/read_range_sensor/read_range_sensor.py
+++ examples/read_range_sensor/read_range_sensor.py
@@ -8,18 +8,7 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 
 vl53 = adafruit_vl53l4cx.VL53L4CX(i2c)
 
-# OPTIONAL: can set non-default values
-vl53.inter_measurement = 0
-vl53.timing_budget = 200
-
 print("VL53L4CX Simple Test.")
-print("--------------------")
-model_id, module_type = vl53.model_info
-print("Model ID: 0x{:0X}".format(model_id))
-print("Module Type: 0x{:0X}".format(module_type))
-print("Timing Budget: {}".format(vl53.timing_budget))
-print("Inter-Measurement: {}".format(vl53.inter_measurement))
-print("--------------------")
 
 vl53.start_ranging()
 
